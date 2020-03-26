f243c4ed253ef65499177729b3a26be95892928c
Phi Huy Hoang <luumanhlapdi@gmail.com>
2020-03-26 18:36:11 +0100

    adding intinal files

---
+++ README.md
@@ -0,0 +1,15 @@
+# CORONA WORLD MAP TRACKING
+
+## Overview
+
+Tracking Corona cases per country via public API.
+
+## Technology stacks
+
+- Javascript
+- Python
+- HTML/CSS
+
+## Live demo:
+
+Click on [here](http://ricecorona.herokuapp.com)
---
+++ run.py
@@ -0,0 +1,31 @@
+from flask import Flask, escape, request, render_template, url_for
+import requests
+import os
+app = Flask(__name__)
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+@app.route('/')
+def index():
+    return render_template('flash.html')
+
+@app.route('/home')
+def home():
+    api = 'https://corona.lmao.ninja/countries'
+    r = requests.get(api)
+    countries = r.json()
+    return render_template('index.html', countries=countries)
+
+if __name__ == '__main__':
+    app.run()
